f5b88bdb7b4055b1d2f0d7c4299c633b7bf5282c
yang-datong <yangjing3@xiaohongshu.com>
2023-06-27 12:54:38 +0800

    修复错误

--- config_file/config_docker.fish
+++ config_file/config_docker.fish
@@ -1,25 +1,43 @@
 if status is-interactive
-    export LD_LIBRARY_PATH='./build/lib'
-    set com 'com.ss.android.ugc.aweme'
-    alias phone='echo 15886670991'
-    alias cl='clear'
-    alias cat='batcat -p'
-    alias vim='nvim'
-    alias rm='trash'
-    alias rm-rf='rm -rf'
-    alias du='du -sh'
-    alias ifconfig="ifconfig | grep inet | grep -v 'inet6\|127.0.0.1'"
-    alias pping="httping -x 127.0.0.1:7890"
-    alias diff='colordiff -w'
-    alias clang='clang -std=c11 -Wall'
-    alias clang++='clang++ -std=c++11 -Wall'
-    alias gcc='gcc -std=c11 -Wall'
-    alias g++='g++ -std=c++11 -Wall'
-    alias chown='sudo chown'
-    alias chmod='sudo chmod'
-    alias apt='sudo apt'
+
+set -x PATH $PATH /usr/local/Cellar
+set -x PATH $PATH $HOME/Library/Python/3.9/bin
+set -x PATH $PATH /usr/local/opt/binutils/bin
+set -x PATH $PATH /usr/local/texlive/2022/bin/universal-darwin/
+set -x PATH $PATH $HOME/Library/Android/sdk/ndk/21.1.6352462
+
+set -x CLOUD $HOME/Library/Mobile\ Documents/com~apple~CloudDocs
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+
+#set com 'com.ss.android.ugc.aweme'
+
+#brew install homebrew/cask-versions/adoptopenjdk8
+
+alias mkdir='mkdir -p'
+alias gdb='/usr/local/Cellar/gdb/12.1/bin/gdb'
+alias phone='echo 15886670991'
+alias ip='curl -s -m 5 https://ipleak.net/json/'
+alias cl='clear'
+alias pip='pip3.9'
+alias python='python3.9'
+alias python3='python3.9'
+alias cat='bat -p'
+alias reboot='sudo reboot'
+alias shutdown='sudo shutdown'
+alias vim='nvim'
+alias rm='trash'
+alias rm-rf='rm -rf'
+alias du='du -sh'
+alias ifconfig="ifconfig | grep inet | grep -v 'inet6\|127.0.0.1'"
+alias pping="httping -x 127.0.0.1:7890"
+alias diff='colordiff -w' #brew install colordiff
+alias clang='clang -std=c11 -Wall'
+alias clang++='clang++ -std=c++11 -Wall'
+alias gcc='gcc -std=c11 -Wall'
+alias g++='g++ -std=c++11 -Wall'
+
 end
 
 function fish_prompt
-    printf '\e[0;91mdocker-$ '
+    printf '\e[0;91mLINUX-$ '
 end
