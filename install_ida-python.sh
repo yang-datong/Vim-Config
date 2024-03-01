68cd5a56a828f259c71abd75c5d6d904aa4f99e1
yangjing <546229768@qq.com>
2024-03-01 15:19:39 +0800

    修改install_ida-python.sh

--- install_ida-python.sh
+++ install_ida-python.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+#https://www.debugwar.com/article/activate-IDAPython-with-wine-IDA-under-linux
+#
 if [ ! -x "$(command -v wine)" ];then
 	echo "wine not installed ....."
 	exit
 fi
 
-if [ ! -f "python-3.12.2-embed-amd64.zip" ];then
-	wget https://www.python.org/ftp/python/3.12.2/python-3.12.2-embed-amd64.zip
+if [ ! -f "python-3.10.2-embed-amd64.zip" ];then
+	wget https://www.python.org/ftp/python/3.10.2/python-3.10.2-embed-amd64.zip
 fi
 
 D="$HOME/.wine/drive_c/Program Files/Python3"
 
 if [ ! -d "$D" ];then
 	mkdir "$D"
-	unzip python-3.12.2-embed-amd64.zip -d "$D"
+	unzip python-3.10.2-embed-amd64.zip -d "$D"
 fi
 
-echo "执行命令：wine regedit , 然后在 HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment,PATH选项中最后添加 ;C:\Program Files\Python3"
+echo "执行命令：wine regedit , 然后在 HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment,PATH选项中最后添加 ;C:\Program Files\Python3;C:\Program Files\Python3\Scripts"
 
 echo "然后在 HKEY_CURRENT_USER\Software\Hex-Rays\IDA, 添加 Python3TargetDLL 为我们上面的python目录下的python3.dll绝对路径 C:\Program Files\Python3\python3.dll"
 
@@ -34,10 +36,14 @@ fi
 wine python.exe ./get-pip.py
 
 #设置python库的搜索路径
-echo "./Lib/site-packages/" >> "$HOME/.wine/drive_c/Program Files/Python3/python312._pth"
+echo "./Lib/site-packages/" >> "$HOME/.wine/drive_c/Program Files/Python3/python310._pth"
 
 #安装Microsoft Visual C++ redistributable及安装，最新版本的 Microsoft Visual C++
-sudo apt install winetricks && sudo winetricks --self-update && winetricks vcrun2017
-#这里可能会发送错误，多跑几次
+#sudo apt install winetricks && sudo winetricks --self-update && winetricks vcrun2017
+#这里可能会发送错误，多跑几次（出问题了再运行）
 
-wine python.exe -m pip install yara keystone-engine six
+wine python.exe -m pip install keystone-engine six yara
+
+#下面的命令主要是针对（出问题了再运行）:使用Apply patches to保存修改时提示patching canceled..
+#https://blog.csdn.net/FigBB/article/details/134856961
+#cp $HOME/.wine/drive_c/Program\ Files/Python3/Lib/site-packages/PyQt5/sip.cp310-win_amd64.pyd $HOME/IDA_Pro_7.7/python/3/PyQt5/sip.pyd
