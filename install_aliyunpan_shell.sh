7307674e4f66ac45f0a68a90abf82d425d99fbb3
yangjing <546229768@qq.com>
2024-03-10 22:54:43 +0800

    修正install_aliyunpan_shell.sh，tools/ubuntu_move_windows脚本文件

--- install_aliyunpan_shell.sh
+++ install_aliyunpan_shell.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-
-#这个下载工具是阿里云盘的shell操作三方工具。优点：
-#1. 阿里云盘下载超过100M的文件需要下载客户端，这个可以突破限制
-#2. 这个工具的下载速度可达 10-20 M每秒
-
-#使用
-#获取refresh_token : 
-#1. 先登陆网页版阿里云盘：
-#2. 控制台执行：JSON.parse(localStorage.getItem("token")).refresh_token
-#3. 项目shell执行
-
-#$ ./aliyunpan
-#aliyunpan > login -RefreshToken=${refresh_token}
-#aliyunpan:/ tickstep$ download IMG_0106.JPG
-#aliyunpan:/ tickstep$ upload /Users/tickstep/Downloads/apt.zip /tmp
+#https://github.com/tickstep/aliyunpan
 
 #Check OS System
 check_os(){
@@ -42,3 +28,24 @@ wget https://github.com/tickstep/aliyunpan/releases/download/v0.2.7/${DIR}.zip -
 unzip /tmp/tmp.zip -d $HOME/
 
 cd $HOME/$DIR
+
+
+cat << EOF
+这个下载工具是阿里云盘的shell操作三方工具。优点：
+- 阿里云盘下载超过100M的文件需要下载客户端，这个可以突破限制
+- 这个工具的下载速度可达 10-20 M每秒
+
+使用：
+1. 先登陆网页版阿里云盘，获取refresh_token：https://www.aliyundrive.com/drive/file/backup
+2. 地址栏输入：javascript:JSON.parse(localStorage.getItem("token")).refresh_token
+2. (或者）控制台执行：JSON.parse(localStorage.getItem("token")).refresh_token
+3. 项目shell执行
+
+$ ./aliyunpan login -RefreshToken=8072a0cbb11848598aca4d879aa86000
+
+# 下载操作：
+$ ./aliyunpan
+aliyunpan:/ tickstep$ download IMG_0106.JPG
+# 上传操作：
+aliyunpan:/ tickstep$ upload /Users/tickstep/Downloads/apt.zip /tmp
+EOF
