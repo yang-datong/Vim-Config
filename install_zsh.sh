517b32fdfc3a33ff8fe335273a73cf92a9be585c
yangjing <546229768@qq.com>
2024-04-27 00:06:04 +0800

    修复install_zsh.sh问题

--- install_zsh.sh
+++ install_zsh.sh
@@ -12,32 +12,30 @@ check_os(){
 }
 
 write(){
-	$cc install zsh
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	local ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+	if [ ! -x "$(command -v zsh)" ];then
+		echo "zsh installing ....."
+		$cc install zsh && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	fi
 
+	local ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 	local plug_dir=${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
 	if [ ! -d $plug_dir ];then
 		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $plug_dir
 	fi
 
-	local plug_dir=${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+	local plug_dir=${ZSH_CUSTOM}/plugins/zsh-autosuggestions
 	if [ ! -d $plug_dir ];then
 		git clone https://github.com/zsh-users/zsh-autosuggestions $plug_dir
 	fi
 
-	$cc install trash-cli #install rm foo -> mv foot foot-bck
-	pip3 install trash-cli
-	$cc install bat
-	$cc install colordiff
+	#因为zsh很可能会是临时用在其他终端中，那么安装这些东西可能会有写配置问题
+	#$cc install trash-cli #install rm foo -> mv foot foot-bck
+	#pip3 install trash-cli
+	#$cc install bat
+	#$cc install colordiff
 
-	if [ ! -d $HOME/autojump ];then
-		git clone https://github.com/wting/autojump.git $HOME/autojump
-	fi
-
-	#echo "IyBQdXQgeW91ciBjdXN0b20gdGhlbWVzIGluIHRoaXMgZm9sZGVyLgojIFNlZTogaHR0cHM6Ly9naXRodWIuY29tL29obXl6c2gvb2hteXpzaC93aWtpL0N1c3RvbWl6YXRpb24jb3ZlcnJpZGluZy1hbmQtYWRkaW5nLXRoZW1lcwojIAojIEV4YW1wbGU6CgojUFJPTVBUPSIleyRmZ1tyZWRdJX0lbiV7JHJlc2V0X2NvbG9yJX1AJXskZmdbYmx1ZV0lfSVtICV7JGZnW3llbGxvd10lfSV+ICV7JHJlc2V0X2NvbG9yJX0lJSAiCgpQUk9NUFQ9JyUoPywleyRmZ1t3aGl0ZV0lfSwleyRmZ1tyZWRdJX0pJCAlICcKUlBTMT0nJXskZmdbd2hpdGVdJX0lMn4kKGdpdF9wcm9tcHRfaW5mbyknCgpleHBvcnQgTFNDT0xPUlM9ImV4ZnhjeGR4YnhieGJ4YnhieGJ4YngiCmV4cG9ydCBMU19DT0xPUlM9ImRpPTM0OzQwOmxuPTM1OzQwOnNvPTMyOzQwOnBpPTMzOzQwOmV4PTMxOzQwOmJkPTMxOzQwOmNkPTMxOzQwOnN1PTMxOzQwOnNnPTMxOzQwOnR3PTMxOzQwOm93PTMxOzQwOiIK" | base64 -d > ${ZSH_CUSTOM}/themes/example.zsh-theme
-	echo "IyBQdXQgeW91ciBjdXN0b20gdGhlbWVzIGluIHRoaXMgZm9sZGVyLgojIFNlZTogaHR0cHM6Ly9naXRodWIuY29tL29obXl6c2gvb2hteXpzaC93aWtpL0N1c3RvbWl6YXRpb24jb3ZlcnJpZGluZy1hbmQtYWRkaW5nLXRoZW1lcwojCiMgRXhhbXBsZToKCiNQUk9NUFQ9IiV7JGZnW3JlZF0lfSVuJXskcmVzZXRfY29sb3IlfUAleyRmZ1tibHVlXSV9JW0gJXskZmdbeWVsbG93XSV9JX4gJXskcmVzZXRfY29sb3IlfSUlICIKUFJPTVBUPSIkZmdbd2hpdGVdJCAiClJQUk9NUFQ9IiV7JGZnW3JlZF0lfSVuJXskcmVzZXRfY29sb3IlfUAleyRmZ1tibHVlXSV9JW0gJXskZmdbeWVsbG93XSV9JX4gJXskcmVzZXRfY29sb3IlfSUgIgo="  | base64 -d > ${ZSH_CUSTOM}/themes/example.zsh-theme
-	cp $SH_FOOT/xx.zshrc $HOME/.zshrc
+	echo "UFJPTVBUPSIlRnsjZmZmZmZmfSQgJWYiClJQUk9NUFQ9IiV7JGZnW3JlZF0lfSVuJXskcmVzZXRfY29sb3IlfUAleyRmZ1tibHVlXSV9JW0gJXskZmdbeWVsbG93XSV9JX4gJXskcmVzZXRfY29sb3IlfSUgIgo="  | base64 -d > ${ZSH_CUSTOM}/themes/example.zsh-theme
+	cp $SH_FOOT/config_file/shell/xx.zshrc $HOME/.zshrc
 }
 
 check_os
