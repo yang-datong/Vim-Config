a5ed68af5ace6dd409e2f438eaa23b63919a9ca1
yangjing <546229768@qq.com>
2024-06-03 12:34:14 +0800

    Update -> install_base.sh

--- install_base.sh
+++ install_base.sh
@@ -15,7 +15,8 @@ main(){
 	pushd $SH_FOOT
 	#Check OS System
 	if [ "$(uname)" == "Linux" ];then
-		sudo add-apt-repository ppa:savoury1/python & sudo apt update
+		sudo add-apt-repository ppa:deadsnakes/ppa
+		sudo apt update
 		sudo apt install -y file passwd python3.10 android-sdk-platform-tools #android-tools-adb
 	elif [ "$(uname)" == "Darwin" ];then
 		if [ ! -x "$(command -v brew)" ];then ./install_brew.sh;fi
