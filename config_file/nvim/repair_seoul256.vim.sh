086959edfcc6f698cc41f90084a5ee9ecf1ec57d
yangjing <546229768@qq.com>
2024-11-14 04:25:41 +0800

    Auto commit from[/usr/local/bin/save-work]

--- config_file/nvim/repair_seoul256.vim.sh
+++ config_file/nvim/repair_seoul256.vim.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-if [ -f $HOME/.config/nvim/plugged/seoul256.vim/colors/seoul256.vim ];then
-	sed -i ".bck" 's/4B4B4B/2a2a2a/' $HOME/.config/nvim/plugged/seoul256.vim/colors/seoul256.vim
+config_file="$HOME/.config/nvim/plugged/seoul256.vim/colors/seoul256.vim"
+if [ -f $config_file ]; then
+	if [ "$(uname)" == "Darwin" ]; then
+		sed -i ".bck" 's/4B4B4B/2a2a2a/' $config_file
+	else
+		sed -i 's/4B4B4B/2a2a2a/' $config_file
+	fi
 else
 	echo -e "\033[31mNot found seoul256.vim file\033[0m"
 fi
