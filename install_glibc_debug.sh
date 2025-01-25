96423bbdea0f92669e66333685b5e612ad12735f
yangjing <546229768@qq.com>
2025-01-26 01:11:00 +0800

    Auto commit from[/usr/local/bin/save-work]

--- install_glibc_debug.sh
+++ install_glibc_debug.sh
@@ -51,11 +51,16 @@ run(){
 	# 配置构建，指定安装目录（无法关闭静态库的编译，只能关闭动态库）
 	../configure --prefix=$HOME/glibc-${version} --enable-debug --disable-werror --host=x86_64-linux-gnu
 	make clean -j$(nproc)
-	make -j$(nproc) CFLAGS="-U_FORTIFY_SOURCE -O2 -g3 -fno-stack-protector" && make install
+	make -j$(nproc) CFLAGS="-U_FORTIFY_SOURCE -Og -g3 -fno-stack-protector" && make install
+	#NOTE: Glibc不允许编译时，完全关闭优化，比如-O0，这会报错：
+	#./../include/libc-symbols.h:75:3: error: #error "glibc cannot be compiled without optimization" 75 | # error "glibc cannot be compiled without optimization"
+	#NOTE: 不需要进行-fno-inline-functions刻意的关闭掉内联，-Og已经提供了很好的调试环境
 
-	echo -e "\033[33m注意！使用方式如下：
-	1. 编译时手动替换系统路径下的libc.so: g++ -Wl,--rpath=PATH/glibc-2.39/lib/ demo.cpp
-	2. apt install patchelf && patchelf --set-rpath $HOME/glibc-2.39/lib a.out
+
+	echo -e "\033[33m
+	编译文件已输出在$HOME/glibc-${version}，注意！使用方式如下（下面两种方式都可以）：
+	1. 编译时手动替换系统路径下的libc.so: g++ -Wl,--rpath=$HOME/glibc-${version}/lib/ demo.cpp
+	2. apt install patchelf && patchelf --set-rpath $HOME/glibc-${version}/lib a.out
 
 	最后通过ldd查看二进制文件链接是否为自己编译路径的libc.so.6\033[0m"
 
