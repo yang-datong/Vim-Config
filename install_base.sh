eadc9e452b0cc1240b2be50058c00cf9747720f6
yang-datong <72976737+yang-datong@users.noreply.github.com>
2025-02-26 03:46:10 +0800

    Update install_base.sh

--- install_base.sh
+++ install_base.sh
@@ -19,7 +19,7 @@ main(){
 			sudo add-apt-repository ppa:deadsnakes/ppa -y
 		fi
 		sudo apt update
-		sudo apt install -y file passwd python3.10 android-sdk-platform-tools #android-tools-adb
+		sudo apt install -y file passwd python3.10 gcc g++ gdb make cmake android-sdk-platform-tools #android-tools-adb
 	elif [ "$(uname)" == "Darwin" ];then
 		if [ ! -x "$(command -v brew)" ];then ./install_brew.sh;fi
 		brew install coreutils python@3.10
@@ -28,6 +28,9 @@ main(){
 
 
 	$cc install curl wget git axel scrcpy
+	if [ ! -f /usr/local/bin/python3.10 ];then
+		sudo ln -s /usr/bin/python3.10 /usr/local/bin/python3.10
+	fi
 	if [ ! -f get-pip.py ];then
 		curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 	fi
