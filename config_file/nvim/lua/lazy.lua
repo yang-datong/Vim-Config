95fdffb5fbac2d7226bf0f9826f665dbd5d64a62
YJ <546229768@qq.com>
2025-08-25 01:10:29 +0800

    Auto commit from[/usr/local/bin/save-work]

---
+++ config_file/nvim/lua/lazy.lua
@@ -0,0 +1,16 @@
+-- 设置 lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- 加载插件配置
+require("lazy").setup("plugins") -- "plugins" 对应 lua/plugins.lua 文件
